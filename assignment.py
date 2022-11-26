ef3dee100b009d9f72d8df71c52b7bce88ae94da
Uchannel92 <uchenna_attah@hotmail.com>
2022-11-26 16:47:13 +0000

    Added the assignment details of the score grade application to the file.

---
+++ assignment.py
@@ -0,0 +1,38 @@
+# Write a program that asks the user to enter 8 numeric tests scores (out of 100 for each test). The program should calculate the average of the 8 numeric scores and then display a corresponding letter grade.
+
+# Write the following functions in the program:
+
+# calc_average - this function should ask the user for 8 test scores and return the average.
+
+# determine_grade - this function should accept a test score average as an argument and return a letter grade for the score based on the following grading scale:
+
+# 90-100A80-89B70-79C60-69DBelow 60F
+
+# convert avg to integer and return the value. Do NOT change
+#return int(avg)
+
+
+# Do NOT change the function header determine_grade(score)
+#def determine_grade(score):
+
+# Convert the numeric score into letter grade
+
+# 90-100: A
+
+# 80-89: B
+
+# 70-79: C
+
+# 60-69: D
+
+# below 60: F
+#return grade
+
+# Do NOT change any of the lines below
+#def main():
+
+
+
+# avg = calc_average()
+# grade = determine_grade(avg)
+# print ("Your average grade is:", grade)
