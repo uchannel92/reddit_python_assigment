22f0eea92ef664a397243e61758072687ce5aba8
Uchannel92 <uchenna_attah@hotmail.com>
2022-11-27 18:05:10 +0000

    More testing which now implements error handling for NaN and numbers less than zero and more than 100

--- test.py
+++ test.py
@@ -25,3 +25,38 @@ def calc_average():
 		return average
 
 result = calc_average()
+
+
+total = 0
+test_score = []
+test_scores_count = 3
+
+while len(test_score) < test_scores_count:
+    
+    try:
+        user_prompt = int(input("Enter a test score more than zero, and less than 100: "))
+    
+        if user_prompt < 0:
+            print(f'Please enter a score higher than zero')
+        
+        elif user_prompt <= 100:
+            test_score.append(user_prompt)
+            
+        elif user_prompt > 100:
+            print(f'Please enter a score less than 100')
+            
+    except ValueError:
+        print(f'Score entered is not a number!')
+        
+    else:
+        print(test_score)
+        
+for number in test_score:
+    total += number
+
+average = round(total / len(test_score))
+
+print(average)
+print(test_score)
+    
+
